a8d9af1577dab9ee36a51df55f9a4da836640eb1
solidtm <richardebboh@gmail.com>
2021-07-16 17:02:50 +0100

    Binary Search Tree implementation in Javascript

--- AlgoWorks/src/com/solid/algolearning/javacode/data_structures/trees/binaryTree.js
+++ AlgoWorks/src/com/solid/algolearning/javacode/data_structures/trees/binaryTree.js
@@ -67,9 +67,85 @@ class BinarySearchTree{
       return false;
     }
 
-    // remove(value){
+//The remove method is a bit more technical. Follow the steps
+     remove(value){
+        if(!this.root){         //if nothing exists in root return false
+         return false;
+        }
 
-    // }
+        let currentNode = this.root;    //Grab the currentNode as this.root first
+        let parentNode = null;          //Initialize a parent node to be used.
+
+      while(currentNode){       //we keep looping until we find a match
+        if(value < currentNode.value){
+        //go left
+           parentNode = currentNode;
+           currentNode = currentNode.left;
+
+        }else if(value > currentNode.value){
+        //go right
+           parentNode = currentNode;
+           currentNode = currentNode.right;
+        }else if(currentNode.value === value){
+         // We have found a match, get to work:
+         //  Option 1: Current node has No right child:
+              if(currentNode.right === null){
+                if(parentNode === null){
+                this.root = currentNode.left;
+                }else{
+                    //if parent > current value, make current left child a child of parent
+                    if(currentNode.value < parentNode.value){
+                        parentNode.left = currentNode.left;
+
+                        //if parent < current value, make left child a right child of parent
+                    }else if(currentNode,value > parentNode.value){
+                        parentNode.right = currentNode.left;
+                    }
+                }
+         // Option 2: Right child which doesn't have a left child
+             }else if(currentNode.right.left === null){
+                if (parentNode === null){
+                    this.root = currentNode.left;
+                }else{
+                    currentNode.right.left = currentNode.left;
+                    //if parent > current, make right child of the left value the parent
+                    if(currentNode.value < parentNode.value){
+                        parentNode.left = currentNode.right;
+
+                     //if parent < current, make right child a right child of the parent
+                    }else if(currentNode.value > parentNode.value){
+                        parentNode.right = currentNode.right;
+                    }
+                }
+         // Option 3: Right child that has a left child
+             }else{
+                   //find the right child's leftmost child
+                   let leftmost = currentNode.right.left;
+                   let leftmostParent = currentNode.right;
+                   while(leftmost.left != null){
+                     leftmostParent = leftmost;
+                     leftmost = leftmost.left;
+                   }
+
+                   //Parent's left subtree is now leftmost's right subtree
+                   leftmostParent.left = leftmost.right;
+                   leftmost.left = currentNode.left;
+                   leftmost.right = currentNode.right;
+
+                   if(parentNode === null){
+                        this.root = leftmost;
+                   }else{
+                        if(currentNode.value < parentNode.value){
+                            parentNode.left = leftmost;
+                        }else if(currentNode.value > parentNode.value){
+                            parentNode.right = leftmost;
+                        }
+                   }
+             }
+             return true;
+        }
+      }
+     }
 }
 
 const bst = new BinarySearchTree();
