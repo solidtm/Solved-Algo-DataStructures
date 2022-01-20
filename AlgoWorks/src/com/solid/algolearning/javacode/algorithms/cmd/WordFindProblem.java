9c37db2e920bac75098df57273987470bda9eb31
Richard <richardebboh@gmail.com>
2022-01-20 14:45:06 -0800

    Easy leetcode CanPlaceFlower problem solution

---
+++ AlgoWorks/src/com/solid/algolearning/javacode/algorithms/cmd/WordFindProblem.java
@@ -0,0 +1,24 @@
+package com.solid.algolearning.javacode.algorithms.cmd;
+
+public class WordFindProblem {
+
+//    Hey guys, so I had a technical interview today, and I couldn't crack this in record time. Can anyone help me out?
+//    The '>' means preceeds, and every element in the array are conditions meaning 'A>B'=> A preceeds B in the output string.
+//    Sorry, I don't have the questions as I was not allowed to copy it
+
+// findWord(["P>E", "E>R", "R>U"]) // PERU
+// findWord(["I>N", "A>I", "P>A", "S>P"]) // SPAIN
+// findWord(["U>N", "G>A", "R>Y", "H>U", "N>G", "A>R"]) // HUNGARY
+// findWord(["I>F", "W>I", "S>W", "F>T"]) // SWIFT
+// findWord(["R>T", "A>L", "P>O", "O>R", "G>A", "T>U", "U>G"]) // PORTUGAL
+// findWord(["W>I", "R>L", "T>Z", "Z>E", "S>W", "E>R", "L>A", "A>N", "N>D", "I>T"]) // SWITZERLAND?
+
+    public static void main(String[] args) {
+
+    }
+
+    static String findWord(String[] wordList){
+        return "";
+    }
+
+}
