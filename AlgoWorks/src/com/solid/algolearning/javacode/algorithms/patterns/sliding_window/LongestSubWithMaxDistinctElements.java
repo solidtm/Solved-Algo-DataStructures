f05cc65975f7dd3fefd8f0b7e130fb4ad3070c78
Richard <richardebboh@gmail.com>
2022-01-20 17:59:22 -0800

    LongestSubWithMaxDistinctElements problem solution

---
+++ AlgoWorks/src/com/solid/algolearning/javacode/algorithms/patterns/sliding_window/LongestSubWithMaxDistinctElements.java
@@ -0,0 +1,38 @@
+package com.solid.algolearning.javacode.algorithms.patterns.sliding_window;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestSubWithMaxDistinctElements {
+    public static void main(String[] args) {
+        System.out.println("Length of the longest substring: " + findLength("araaci", 2));
+        System.out.println("Length of the longest substring: " + findLength("araaci", 1));
+        System.out.println("Length of the longest substring: " + findLength("cbbebi", 3));
+    }
+
+    public static int findLength(String str, int k) {
+        if (str == null || str.length() == 0)
+            throw new IllegalArgumentException();
+
+        int windowStart = 0, maxLength = 0;
+        Map<Character, Integer> charFrequencyMap = new HashMap<>();
+        // in the following loop we'll try to extend the range [windowStart, windowEnd]
+        for (int windowEnd = 0; windowEnd < str.length(); windowEnd++) {
+            char rightChar = str.charAt(windowEnd);
+            charFrequencyMap.put(rightChar, charFrequencyMap.getOrDefault(rightChar, 0) + 1);
+            // shrink the sliding window, until we are left with 'k' distinct characters in the frequency map
+            while (charFrequencyMap.size() > k) {
+                char leftChar = str.charAt(windowStart);
+                charFrequencyMap.put(leftChar, charFrequencyMap.get(leftChar) - 1);
+                if (charFrequencyMap.get(leftChar) == 0) {
+                    charFrequencyMap.remove(leftChar);
+                }
+                windowStart++; // shrink the window
+            }
+            maxLength = Math.max(maxLength, windowEnd - windowStart + 1); // remember the maximum length so far
+        }
+
+        return maxLength;
+    }
+
+}
