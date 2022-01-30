0c1652abd180b2329250a844c21ae8439bacd0b7
solidtm <richardebboh@gmail.com>
2022-01-30 07:37:11 +0100

    Solved Problems on sliding window pattern

---
+++ AlgoWorks/src/com/solid/algolearning/javacode/algorithms/patterns/sliding_window/Problem2_StringAnagram.java
@@ -0,0 +1,67 @@
+package com.solid.algolearning.javacode.algorithms.patterns.sliding_window;
+
+// String Anagrams (hard)#
+// Given a string and a pattern, find all anagrams of the pattern in the given string.
+
+// Every anagram is a permutation of a string. As we know, when we are not allowed to repeat characters while finding permutations of a string, we get N!N! permutations (or anagrams) of a string having NN characters. For example, here are the six anagrams of the string “abc”:
+
+// abc
+// acb
+// bac
+// bca
+// cab
+// cba
+// Write a function to return a list of starting indices of the anagrams of the pattern in the given string.
+
+// Example 1:
+
+// Input: String="ppqp", Pattern="pq"
+// Output: [1, 2]
+// Explanation: The two anagrams of the pattern in the given string are "pq" and "qp".
+// Example 2:
+
+// Input: String="abbcabc", Pattern="abc"
+// Output: [2, 3, 4]
+// Explanation: The three anagrams of the pattern in the given string are "bca", "cab", and "abc".
+import java.util.*;
+public class Problem2_StringAnagram {
+
+    public static List<Integer> findStringAnagrams(String str, String pattern) {
+        int windowStart = 0, matched = 0;
+        Map<Character, Integer> charFrequencyMap = new HashMap<>();
+        for (char chr : pattern.toCharArray())
+            charFrequencyMap.put(chr, charFrequencyMap.getOrDefault(chr, 0) + 1);
+
+        List<Integer> resultIndices = new ArrayList<Integer>();
+        // our goal is to match all the characters from the map with the current window
+        for (int windowEnd = 0; windowEnd < str.length(); windowEnd++) {
+            char rightChar = str.charAt(windowEnd);
+            // decrement the frequency of the matched character
+            if (charFrequencyMap.containsKey(rightChar)) {
+                charFrequencyMap.put(rightChar, charFrequencyMap.get(rightChar) - 1);
+                if (charFrequencyMap.get(rightChar) == 0)
+                    matched++;
+            }
+
+            if (matched == charFrequencyMap.size()) // have we found an anagram?
+                resultIndices.add(windowStart);
+
+            if (windowEnd >= pattern.length() - 1) { // shrink the window
+                char leftChar = str.charAt(windowStart++);
+                if (charFrequencyMap.containsKey(leftChar)) {
+                    if (charFrequencyMap.get(leftChar) == 0)
+                        matched--; // before putting the character back, decrement the matched count
+                    // put the character back
+                    charFrequencyMap.put(leftChar, charFrequencyMap.get(leftChar) + 1);
+                }
+            }
+        }
+
+        return resultIndices;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+}
