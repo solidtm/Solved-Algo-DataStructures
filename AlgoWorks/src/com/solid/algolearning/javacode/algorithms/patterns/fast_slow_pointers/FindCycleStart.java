0a23da4a3e5fa1d9538ecfe29eb729d65726c767
solidtm <richardebboh@gmail.com>
2022-02-03 04:12:55 +0100

    findCycleStart problem better approach/solution

--- AlgoWorks/src/com/solid/algolearning/javacode/algorithms/patterns/fast_slow_pointers/FindCycleStart.java
+++ AlgoWorks/src/com/solid/algolearning/javacode/algorithms/patterns/fast_slow_pointers/FindCycleStart.java
@@ -11,50 +11,73 @@ public class FindCycleStart {
         }
     }
 
-    public static ListNode findCycleStart(ListNode head) {
-        int cycleLength = 0;
-        // find the LinkedList cycle
-        ListNode slow = head;
+    //faster and more concise approach
+    public static ListNode findCycleStart(ListNode head) { // 3->2->0->-4->  fast = -4, slow = -4, temp = 3;
         ListNode fast = head;
-        while (fast != null && fast.next != null) {
+        ListNode slow = head;
+        ListNode temp = head;
+        if(head == null)
+            return null;
+        while(fast.next != null && fast.next.next != null)
+        {
             fast = fast.next.next;
             slow = slow.next;
-            if (slow == fast) { // found the cycle
-                cycleLength = calculateCycleLength(slow);
-                break;
+            if(slow == fast)
+            {
+                while(temp != slow)
+                {
+                    temp=temp.next;
+                    slow=slow.next;
+                }
+                return temp;
             }
         }
-
-        return findStart(head, cycleLength);
+        return null;
     }
-
-    private static int calculateCycleLength(ListNode slow) {
-        ListNode current = slow;
-        int cycleLength = 0;
-        do {
-            current = current.next;
-            cycleLength++;
-        } while (current != slow);
-
-        return cycleLength;
-    }
-
-    private static ListNode findStart(ListNode head, int cycleLength) {
-        ListNode pointer1 = head, pointer2 = head;
-        // move pointer2 ahead 'cycleLength' nodes
-        while (cycleLength > 0) {
-            pointer2 = pointer2.next;
-            cycleLength--;
-        }
-
-        // increment both pointers until they meet at the start of the cycle
-        while (pointer1 != pointer2) {
-            pointer1 = pointer1.next;
-            pointer2 = pointer2.next;
-        }
-
-        return pointer1;
-    }
+//    public static ListNode findCycleStart(ListNode head) {
+//        int cycleLength = 0;
+//        // find the LinkedList cycle
+//        ListNode slow = head;
+//        ListNode fast = head;
+//        while (fast != null && fast.next != null) {
+//            fast = fast.next.next;
+//            slow = slow.next;
+//            if (slow == fast) { // found the cycle
+//                cycleLength = calculateCycleLength(slow);
+//                break;
+//            }
+//        }
+//
+//        return findStart(head, cycleLength);
+//    }
+//
+//    private static int calculateCycleLength(ListNode slow) {
+//        ListNode current = slow;
+//        int cycleLength = 0;
+//        do {
+//            current = current.next;
+//            cycleLength++;
+//        } while (current != slow);
+//
+//        return cycleLength;
+//    }
+//
+//    private static ListNode findStart(ListNode head, int cycleLength) {
+//        ListNode pointer1 = head, pointer2 = head;
+//        // move pointer2 ahead 'cycleLength' nodes
+//        while (cycleLength > 0) {
+//            pointer2 = pointer2.next;
+//            cycleLength--;
+//        }
+//
+//        // increment both pointers until they meet at the start of the cycle
+//        while (pointer1 != pointer2) {
+//            pointer1 = pointer1.next;
+//            pointer2 = pointer2.next;
+//        }
+//
+//        return pointer1;
+//    }
 
     public static void main(String[] args) {
         ListNode head = new ListNode(1);
