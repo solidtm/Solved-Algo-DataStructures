be4a5386aea8e1d806f997b5c942cb4db2ce784c
solidtm <richardebboh@gmail.com>
2022-02-07 03:08:13 +0100

    ConflictingAppointments problem solution

--- AlgoWorks/src/com/solid/algolearning/javacode/algorithms/patterns/merge_intervals/ConflictingAppointments.java
+++ AlgoWorks/src/com/solid/algolearning/javacode/algorithms/patterns/merge_intervals/ConflictingAppointments.java
@@ -2,6 +2,25 @@ package com.solid.algolearning.javacode.algorithms.patterns.merge_intervals;
 
 import java.util.*;
 
+//Problem Statement #
+//        Given an array of intervals representing ‘N’ appointments, find out if a person can attend all the appointments.
+//
+//        Example 1:
+//
+//        Appointments: [[1,4], [2,5], [7,9]]
+//        Output: false
+//        Explanation: Since [1,4] and [2,5] overlap, a person cannot attend both of these appointments.
+//        Example 2:
+//
+//        Appointments: [[6,7], [2,4], [8,12]]
+//        Output: true
+//        Explanation: None of the appointments overlap, therefore a person can attend all of them.
+//        Example 3:
+//
+//        Appointments: [[4,5], [2,3], [3,6]]
+//        Output: false
+//        Explanation: Since [4,5] and [3,6] overlap, a person cannot attend both of these appointments.
+
 public class ConflictingAppointments {
 
     static class Interval {
