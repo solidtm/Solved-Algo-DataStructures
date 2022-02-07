2f06be5f89c16aa04e04ca60245eef4a217d4d7d
solidtm <richardebboh@gmail.com>
2022-02-07 14:35:15 +0100

    EmployeeFreeTime problem solution

--- AlgoWorks/src/com/solid/algolearning/javacode/algorithms/patterns/merge_intervals/EmployeeWorkingTime.java
+++ AlgoWorks/src/com/solid/algolearning/javacode/algorithms/patterns/merge_intervals/EmployeeWorkingTime.java
@@ -99,8 +99,8 @@ class EmployeeFreeTime {
     public static void main(String[] args) {
 
         List<List<Interval>> input = new ArrayList<>();
-        input.add(new ArrayList<Interval>(Arrays.asList(new Interval(1, 3), new Interval(5, 6))));
-        input.add(new ArrayList<Interval>(Arrays.asList(new Interval(2, 3), new Interval(6, 8))));
+        input.add(new ArrayList<>(Arrays.asList(new Interval(1, 3), new Interval(5, 6))));
+        input.add(new ArrayList<>(Arrays.asList(new Interval(2, 3), new Interval(6, 8))));
         List<Interval> result = EmployeeFreeTime.findEmployeeFreeTime(input);
         System.out.print("Free intervals: ");
         for (Interval interval : result)
@@ -108,9 +108,9 @@ class EmployeeFreeTime {
         System.out.println();
 
         input = new ArrayList<>();
-        input.add(new ArrayList<Interval>(Arrays.asList(new Interval(1, 3), new Interval(9, 12))));
-        input.add(new ArrayList<Interval>(Arrays.asList(new Interval(2, 4))));
-        input.add(new ArrayList<Interval>(Arrays.asList(new Interval(6, 8))));
+        input.add(new ArrayList<>(Arrays.asList(new Interval(1, 3), new Interval(9, 12))));
+        input.add(new ArrayList<>(Arrays.asList(new Interval(2, 4))));
+        input.add(new ArrayList<>(Arrays.asList(new Interval(6, 8))));
         result = EmployeeFreeTime.findEmployeeFreeTime(input);
         System.out.print("Free intervals: ");
         for (Interval interval : result)
@@ -118,9 +118,9 @@ class EmployeeFreeTime {
         System.out.println();
 
         input = new ArrayList<>();
-        input.add(new ArrayList<Interval>(Arrays.asList(new Interval(1, 3))));
-        input.add(new ArrayList<Interval>(Arrays.asList(new Interval(2, 4))));
-        input.add(new ArrayList<Interval>(Arrays.asList(new Interval(3, 5), new Interval(7, 9))));
+        input.add(new ArrayList<>(Arrays.asList(new Interval(1, 3))));
+        input.add(new ArrayList<>(Arrays.asList(new Interval(2, 4))));
+        input.add(new ArrayList<>(Arrays.asList(new Interval(3, 5), new Interval(7, 9))));
         result = EmployeeFreeTime.findEmployeeFreeTime(input);
         System.out.print("Free intervals: ");
         for (Interval interval : result)
