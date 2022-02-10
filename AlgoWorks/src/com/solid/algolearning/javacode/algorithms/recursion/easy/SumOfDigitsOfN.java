1de62f9ba521c2a7798d920444209abeb5a92834
solidtm <richardebboh@gmail.com>
2022-02-10 13:19:11 +0100

    SumOfDigitsOfN problem solution

--- AlgoWorks/src/com/solid/algolearning/javacode/algorithms/recursion/easy/SumOfDigitsOfN.java
+++ AlgoWorks/src/com/solid/algolearning/javacode/algorithms/recursion/easy/SumOfDigitsOfN.java
@@ -9,7 +9,7 @@ public class SumOfDigitsOfN {
     }
 
     static int sumDigits(int n){
-        if(n == 0) return 0;
+        if(n % 10 == n) return n;
 
         int rem = n % 10;
         n = n / 10;
