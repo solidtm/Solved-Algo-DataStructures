9bcd1e73f905e5468b1b5d6c2ba1dabe530c5e67
solidtm <richardebboh@gmail.com>
2022-02-13 12:25:03 +0100

    bubble sort recursive approach

--- AlgoWorks/src/com/solid/algolearning/javacode/algorithms/sorting/BubbleSort.java
+++ AlgoWorks/src/com/solid/algolearning/javacode/algorithms/sorting/BubbleSort.java
@@ -1,5 +1,7 @@
 package com.solid.algolearning.javacode.algorithms.sorting;
 
+import com.sun.org.apache.xpath.internal.objects.XNumber;
+
 import java.util.Arrays;
 
 public class BubbleSort {
@@ -8,16 +10,43 @@ public class BubbleSort {
     public static void main(String[] args) {
         int[] numbers = {34, 7, 65, 2, 8, 1, 2};
 
+        System.out.println(Arrays.toString(bubbleSortIterative(numbers)));
+
+        bubbleSortRecursive(numbers, 0, 0);
+        System.out.println(Arrays.toString(numbers));
+    }
+
+
+    static int[] bubbleSortIterative(int[] numbers){
         for(int i = 0; i < numbers.length; i++){
             for (int j = i + 1; j < numbers.length; j++){
                 if (numbers[i] > numbers[j]){
-                    int temp = numbers[i];
-                    numbers[i] = numbers[j];
-                    numbers[j] = temp;
+                    swap(numbers, i, j);
                 }
             }
         }
 
-        System.out.println(Arrays.toString(numbers));
+        return numbers;
+    }
+
+
+
+    static void bubbleSortRecursive(int[] arr, int start, int end){
+        if(start == 0) return;
+
+        if (end < start){
+            if(arr[start] > arr[end]){
+                swap(arr, start, end);
+            }
+            bubbleSortRecursive(arr, start, end + 1);
+        }else {
+           bubbleSortRecursive(arr, start - 1, end);
+        }
+    }
+
+    static void swap(int[] arr, int start, int end){
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
     }
 }
