dcf57667f80d6ef73e0fbea64b9193616d6c50c3
solidtm <richardebboh@gmail.com>
2022-02-13 12:25:57 +0100

    mergesort in-place sorting approach

--- AlgoWorks/src/com/solid/algolearning/javacode/algorithms/sorting/MergeSort.java
+++ AlgoWorks/src/com/solid/algolearning/javacode/algorithms/sorting/MergeSort.java
@@ -1,59 +1,116 @@
 package com.solid.algolearning.javacode.algorithms.sorting;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class MergeSort {
     public static void main(String[] args) {
-        List<Integer> array = Arrays.asList(20, 35, -15, 7, 55, 1, -22);
-        mergeSort(array);
+        int[] array = new int[]{5, 4, 3, 2, 1};
+        int[] arr = {20, 35, -15, 7, 55, 1, -22};
+        System.out.println(Arrays.toString(mergeSort(array)));
 
-        System.out.println(Arrays.toString(array.toArray()));
+        mergeSortInPlace(arr, 0, arr.length );
+        System.out.println(Arrays.toString(arr));
     }
 
-    public static List<Integer> mergeSort(List<Integer> input){
-        if(input.size() < 2){
+    public static int[] mergeSort(int[] input){
+        if(input.length < 2){
             return input;
         }
 
-        int mid = input.size() / 2;
+        int mid = input.length / 2;
 
-        List<Integer> left = input.subList(0, mid);
-        List<Integer> right = input.subList(mid, input.size());
-
-        System.out.println("left: " + left);
-        System.out.println("right: " + right);
+       int[] left = Arrays.copyOfRange(input, 0, mid);
+       int[] right = Arrays.copyOfRange(input, mid, input.length);
 
         return merge(mergeSort(left), mergeSort(right));
     }
 
-    public static List<Integer> merge(List<Integer> left, List<Integer> right){
+    public static int[] merge(int[] left, int[] right){
         int leftIndex = 0;
         int rightIndex = 0;
-        int size = left.size() + right.size();
-        List<Integer> tempList = new ArrayList<>(size);
+        int k = 0;
+        int size = left.length + right.length;
+       int[] tempList = new int[left.length + right.length];
 
-        while(leftIndex < left.size() && rightIndex < right.size()){
-            if(left.get(leftIndex) <= right.get(rightIndex)){
-                tempList.add(left.get(leftIndex));
+        while(leftIndex < left.length && rightIndex < right.length){
+            if(left[leftIndex] <= right[rightIndex]){
+                tempList[k] = left[leftIndex];
                 leftIndex++;
-            }else{
-                tempList.add(right.get(rightIndex));
+                k++;
+            }else if(left[leftIndex] >= right[rightIndex]){
+                tempList[k] = right[rightIndex];
                 rightIndex++;
+                k++;
             }
+        }
 
-            while(leftIndex < left.size()){
-                tempList.add(left.get(leftIndex));
-                leftIndex++;
-            }
+        while(leftIndex < left.length){
+            tempList[k] = left[leftIndex];
+            leftIndex++;
+            k++;
+        }
 
-            while(rightIndex < right.size()){
-                tempList.add(right.get(rightIndex));
-                rightIndex++;
-            }
+        while(rightIndex < right.length){
+            tempList[k] = right[rightIndex];
+            rightIndex++;
+            k++;
         }
 
         return tempList;
     }
+
+
+
+    public static void mergeSortInPlace(int[] input, int start, int end){
+        if(end - start == 1){
+            return;
+        }
+
+        int mid = start + (end - start) / 2;
+
+        int[] left = Arrays.copyOfRange(input, 0, mid);
+        int[] right = Arrays.copyOfRange(input, mid, end);
+
+        mergeSortInPlace(input, start, mid);
+        mergeSortInPlace(input, mid, end);
+
+        mergeInPlace(input, start, mid, end);
+    }
+
+//    In-place implementation of mergeSort:
+//    1. pass in both the arr and start and end for the array partition
+public static void mergeInPlace(int[] arr, int start, int mid, int end){
+    int leftIndex = start;
+    int rightIndex = mid;
+    int k = 0;
+
+    int[] tempList = new int[end - start];
+
+    while(leftIndex < mid && rightIndex < end){
+        if(arr[leftIndex] <= arr[rightIndex]){
+            tempList[k] = arr[leftIndex];
+            leftIndex++;
+        }else{
+            tempList[k] = arr[rightIndex];
+            rightIndex++;
+        }
+        k++;
+
+    }
+
+    while(leftIndex < mid){
+        tempList[k] = arr[leftIndex];
+        leftIndex++;
+        k++;
+    }
+
+    while(rightIndex < end){
+        tempList[k] = arr[rightIndex];
+        rightIndex++;
+        k++;
+    }
+
+
+    System.arraycopy(tempList, 0, arr, start, tempList.length);
+  }
 }
