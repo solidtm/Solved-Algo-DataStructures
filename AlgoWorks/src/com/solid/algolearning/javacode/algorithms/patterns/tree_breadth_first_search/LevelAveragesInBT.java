5798201ab388a9f5c645bea268606504e8920bec
richardmmakuaebo <mrichard@youverify.co>
2022-02-20 13:52:26 +0100

    BFS LevelAveragesInBT problem solution

---
+++ AlgoWorks/src/com/solid/algolearning/javacode/algorithms/patterns/tree_breadth_first_search/LevelAveragesInBT.java
@@ -0,0 +1,47 @@
+package com.solid.algolearning.javacode.algorithms.patterns.tree_breadth_first_search;
+
+//Problem Statement#
+//        Given a binary tree, populate an array to represent the averages of all of its levels.
+
+import java.util.*;
+public class LevelAveragesInBT {
+    public static List<Double> findLevelAverages(TreeNode root) {
+        List<Double> result = new ArrayList<>();
+        if (root == null)
+            return result;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            double levelSum = 0;
+
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode currentNode = queue.poll();
+                levelSum += currentNode.val;  // add the node's value to the running sum
+
+                // insert the children of current node to the queue
+                if (currentNode.left != null)
+                    queue.offer(currentNode.left);
+                if (currentNode.right != null)
+                    queue.offer(currentNode.right);
+            }
+            result.add(levelSum / levelSize); // append the current level's average to the result array
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(12);
+        root.left = new TreeNode(7);
+        root.right = new TreeNode(1);
+        root.left.left = new TreeNode(9);
+        root.left.right = new TreeNode(2);
+        root.right.left = new TreeNode(10);
+        root.right.right = new TreeNode(5);
+        List<Double> result = findLevelAverages(root);
+        System.out.print("Level averages are: " + result);
+    }
+}
