8eb3bdd89d365f03c79cca7e565146fe33a9ea55
richardmmakuaebo <mrichard@youverify.co>
2022-02-24 16:38:18 +0100

    subset pattern problem solutions

---
+++ AlgoWorks/src/com/solid/algolearning/javacode/algorithms/patterns/subsets/Subsets.java
@@ -0,0 +1,16 @@
+package com.solid.algolearning.javacode.algorithms.patterns.subsets;
+//Problem Statement#
+//        Given a set with distinct elements, find all of its distinct subsets.
+//
+//        Example 1:
+//
+//        Input: [1, 3]
+//        Output: [], [1], [3], [1,3]
+//        Example 2:
+//
+//        Input: [1, 5, 3]
+//        Output: [], [1], [5], [3], [1,5], [1,3], [5,3], [1,5,3]
+
+public class Subsets {
+
+}
