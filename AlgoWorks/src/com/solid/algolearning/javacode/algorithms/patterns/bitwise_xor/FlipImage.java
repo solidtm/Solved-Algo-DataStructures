67d57fe0a7e1c260798cde863c8b947de0cbb697
richardmmakuaebo <mrichard@youverify.co>
2022-02-27 19:53:45 +0100

    bug fixes

--- AlgoWorks/src/com/solid/algolearning/javacode/algorithms/patterns/bitwise_xor/FlipImage.java
+++ AlgoWorks/src/com/solid/algolearning/javacode/algorithms/patterns/bitwise_xor/FlipImage.java
@@ -12,10 +12,10 @@ public class FlipImage {
 
        for(int[] row : image){
            //reverse this array
-           for (int i = 0; i < (image[0].length + 1); i++) {
+           for (int i = 0; i < (image[0].length + 1) / 2; i++) {
                //swap
                int temp = row[i] ^ 1;
-               row[i] = row[image[0].length - i - 1];
+               row[i] = row[image[0].length - i - 1] ^ 1;
                row[image[0].length] = temp;
            }
        }
